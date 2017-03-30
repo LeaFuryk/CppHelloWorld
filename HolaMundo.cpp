fc1d9dfe1918bd68d2e4ee9f60329b7b88bd3039
LeaFuryk <leandrofuryk@gmail.com>
2017-03-30 12:09:06 -0300

    Add files via upload

---
+++ HolaMundo.cpp
@@ -0,0 +1,14 @@
+/*
+*HolaMundo
+*Imprime en consola "Hola Mundo"
+*Leandro Furyk
+*26/03/2017
+*/
+
+#include <iostream>
+
+int main(int args, const char * argv[]) {
+
+	std::cout << "Hello World! \n";	
+	return 0;
+}
